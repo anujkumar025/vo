8bd827e041687f66be2f531217b7147aff8e61b2
anujkumar025 <ffriction73@gmail.com>
2023-08-13 21:25:05 +0530

    adding comment to check

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-
+//adding this comment to check
 const db = mysql.createPool({
     host: "localhost", 
     user:"root",
